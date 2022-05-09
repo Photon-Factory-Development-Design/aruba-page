5d8b71de66bed45c2e21ae2b0b9ea2f504882d9d
TalentWebDev <49565243+talentwebdev@users.noreply.github.com>
2022-05-08 22:45:43 -0400

    [+] add assets images

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-export { default as BackgroundContainer } from './BackgroundContainer';
-export { default as Typography } from './Typography';
-export { default as Link } from './Link';
-export { default as Stepper } from './Stepper';
-export { default as Carousel } from './Carousel';
-export { default as ProductImage } from './ProductImage';
-export { default as ProductDetail } from './ProductDetail';
-export { default as QuestionItemContainer } from './QuestionItemContainer';
-export { default as Hero } from './Hero/Hero.jsx';
--- src/containers/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Header } from './Header';
-export { default as Logo } from './Logo';
-export { default as IngrdientsContainer } from './IngredientsContainer';
-export { default as QuizContainer } from './QuizContainer';
-export { default as ProductDetailDialog } from './ProductDetailDialog';
-export { default as IngredientDetailDialog } from './IngredientDetailDialog';
