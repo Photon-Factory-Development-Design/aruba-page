75bda993f691723e1a5055a0fa87a138263b3eb3
TalentWebDev <49565243+talentwebdev@users.noreply.github.com>
2022-05-16 13:23:22 -0400

    fix - 1

--- src/components/Steps/ProductsStep/ProductsStep.jsx
+++ src/components/Steps/ProductsStep/ProductsStep.jsx
@@ -17,7 +17,7 @@ const ProductsStep = (props) => {
             onGoBack={onGoBack}>
             <div
                 className={
-                    'container grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 grid max-w-fit m-auto'
+                    'container grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 grid max-w-fit m-auto place-items-center'
                 }>
                 {(recommends || []).map((product) => (
                     <ProductCard
